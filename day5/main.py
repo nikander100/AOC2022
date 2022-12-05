931d67ccdb5ab5aad8c09222edf5b62ab43384c6
Nikander van der Schee <nvan-der@student.codam.nl>
2022-12-05 23:12:08 +0100

    Update main.py

--- day5/main.py
+++ day5/main.py
@@ -1,37 +1,44 @@
 with open("input") as f:
 	fContent = f.read();
 
-
-stackOne   = ["W", "M", "L", "F"];
-stackTwo   = ["B", "Z", "V", "M", "F"]
-stackThree = ["H", "V", "R", "S", "L", "Q"]
-stackFour  = ["F", "S", "V", "Q", "P", "M", "T", "J"]
-stackFive  = ["L", "S", "W"]
-stackSix   = ["F", "V", "P", "M", "R", "J", "W"]
-stackSeven = ["J", "Q", "C", "P", "N", "R", "F"]
-stackEight = ["V", "H", "P", "S", "Z", "W", "R", "B"]
-stackNine  = ["B", "M", "J", "C", "G", "H", "Z", "W"]
-
-stack = [stackOne.copy(), stackTwo.copy(), stackThree.copy(), stackFour.copy(), stackFive.copy(), stackSix.copy(), stackSeven.copy(), stackEight.copy(), stackNine.copy()]
-stackPartTwo = [stackOne.copy(), stackTwo.copy(), stackThree.copy(), stackFour.copy(), stackFive.copy(), stackSix.copy(), stackSeven.copy(), stackEight.copy(), stackNine.copy()]
-
 inputStacks, inputFile = fContent.split("\n\n")
+
+# Stack Creator
+stackInput = []
+stackInputRow = []
+for inputLine in inputStacks.split("\n"):
+	for x in range(1, 34, 4):
+		stackInputRow.append(inputLine[x])
+		if stackInputRow[-1] == " ":
+			stackInputRow[-1] = ""
+	stackInput.append(stackInputRow)
+	stackInputRow = []
+stackInput.pop()
+
+stacks = [[]for _ in range(9)]
+stacksPartTwo = [[]for _ in range(9)]
+for inputSortedLine in reversed(stackInput):
+	for j, crate in enumerate(inputSortedLine):
+		stacks[j].append(crate)
+
+for i, stack in enumerate(stacks):
+	stacks[i] = [x for x in stack if x]
+	stacksPartTwo[i] = [x for x in stack if x]
+
+# # PushSwap
 for inputLine in inputFile.split("\n"):
 	_, moveAmount, _, fromStack, _, toStack = inputLine.split(" ")
 	for x in range(int(moveAmount), 0, -1):
-		stack[int(toStack) - 1].append(stack[int(fromStack) -1].pop())
+		stacks[int(toStack)].append(stacks[int(fromStack)].pop())
+	break
 	for x in range(int(moveAmount), 0, -1):
-		stackPartTwo[int(toStack) - 1].append(stackPartTwo[int(fromStack) -1].pop(-x))
-# for inputLine in stacks.split("\n"):
-# 	for inputCrate in inputLine.split("  "):
-# 		# if inputCrate == " ":
-# 		# 	inputCrate.pop()
-# 		print("["+inputCrate+"]")
-# 	break;
+		stacksPartTwo[int(toStack) - 1].append(stacksPartTwo[int(fromStack) -1].pop(-x))
+
+# Output
 print("Part 1")
 for x in stack:
 	print(x)
 print()
 print("Part 2")
-for x in stackPartTwo:
+for x in stacksPartTwo:
 	print(x)
