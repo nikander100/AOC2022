0ee9230f7ed72a61fa649b75cf623ca8e83cb93b
Nikander van der Schee <nvan-der@student.codam.nl>
2022-12-05 18:38:03 +0100

    part2 but still need to pretty + input

--- day5/main.py
+++ day5/main.py
@@ -11,28 +11,27 @@ stackSix   = ["F", "V", "P", "M", "R", "J", "W"]
 stackSeven = ["J", "Q", "C", "P", "N", "R", "F"]
 stackEight = ["V", "H", "P", "S", "Z", "W", "R", "B"]
 stackNine  = ["B", "M", "J", "C", "G", "H", "Z", "W"]
-stack = [stackOne, stackTwo, stackThree, stackFour, stackFive, stackSix, stackSeven, stackEight, stackNine]
+
+stack = [stackOne.copy(), stackTwo.copy(), stackThree.copy(), stackFour.copy(), stackFive.copy(), stackSix.copy(), stackSeven.copy(), stackEight.copy(), stackNine.copy()]
+stackPartTwo = [stackOne.copy(), stackTwo.copy(), stackThree.copy(), stackFour.copy(), stackFive.copy(), stackSix.copy(), stackSeven.copy(), stackEight.copy(), stackNine.copy()]
 
 inputStacks, inputFile = fContent.split("\n\n")
 for inputLine in inputFile.split("\n"):
 	_, moveAmount, _, fromStack, _, toStack = inputLine.split(" ")
 	for x in range(int(moveAmount), 0, -1):
 		stack[int(toStack) - 1].append(stack[int(fromStack) -1].pop())
+	for x in range(int(moveAmount), 0, -1):
+		stackPartTwo[int(toStack) - 1].append(stackPartTwo[int(fromStack) -1].pop(-x))
 # for inputLine in stacks.split("\n"):
 # 	for inputCrate in inputLine.split("  "):
 # 		# if inputCrate == " ":
 # 		# 	inputCrate.pop()
 # 		print("["+inputCrate+"]")
 # 	break;
+print("Part 1")
 for x in stack:
 	print(x)
-
-# ['B', 'L', 'F', 'S', 'V', 'R', 'S', 'L', 'H', 'V']
-# ['W', 'J', 'B', 'P', 'J', 'M', 'F', 'P', 'W', 'M', 'R', 'R']
-# ['F', 'W']
-# ['V', 'S', 'C', 'B']
-# ['V', 'J', 'H', 'J', 'M', 'H', 'S']
-# ['Q', 'R', 'P', 'M', 'G', 'Z', 'Z', 'F']
-# ['F', 'C', 'Q', 'L', 'P', 'T', 'V', 'W', 'Q', 'Z']
-# ['N', 'W']
-# ['M']
+print()
+print("Part 2")
+for x in stackPartTwo:
+	print(x)
