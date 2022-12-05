bfaa104072eaf2d39951d9392e9c5af3e1e35993
Nikander van der Schee <nvan-der@student.codam.nl>
2022-12-05 18:13:12 +0100

    upate p1

--- day5/main.py
+++ day5/main.py
@@ -16,7 +16,7 @@ stack = [stackOne, stackTwo, stackThree, stackFour, stackFive, stackSix, stackSe
 inputStacks, inputFile = fContent.split("\n\n")
 for inputLine in inputFile.split("\n"):
 	_, moveAmount, _, fromStack, _, toStack = inputLine.split(" ")
-	for x in range(0, int(moveAmount)):
+	for x in range(int(moveAmount), 0, -1):
 		stack[int(toStack) - 1].append(stack[int(fromStack) -1].pop())
 # for inputLine in stacks.split("\n"):
 # 	for inputCrate in inputLine.split("  "):
@@ -26,3 +26,13 @@ for inputLine in inputFile.split("\n"):
 # 	break;
 for x in stack:
 	print(x)
+
+# ['B', 'L', 'F', 'S', 'V', 'R', 'S', 'L', 'H', 'V']
+# ['W', 'J', 'B', 'P', 'J', 'M', 'F', 'P', 'W', 'M', 'R', 'R']
+# ['F', 'W']
+# ['V', 'S', 'C', 'B']
+# ['V', 'J', 'H', 'J', 'M', 'H', 'S']
+# ['Q', 'R', 'P', 'M', 'G', 'Z', 'Z', 'F']
+# ['F', 'C', 'Q', 'L', 'P', 'T', 'V', 'W', 'Q', 'Z']
+# ['N', 'W']
+# ['M']
